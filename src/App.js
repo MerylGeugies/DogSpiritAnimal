07114cf1a0ee54d86c466d013740f6f8c3aa4dae
Leon <Yousif@MBPvanLiesbeth.localdomain>
2019-07-24 13:49:29 +0200

    DogList23

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Buttons from './components/Buttons'
 import store from './store';
 import { Provider } from 'react-redux';
-import DogList from './component/DogList'
+import DogList from './component/DogList';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <main>
-          <DogList></DogList>
+        <DogList></DogList>
           <h1>Dog Spirit Animal APP</h1>
           <Buttons buttonText={'Learn dog breeds'} buttonClass={"learnbutton"} handleclick={() => alert('let\'s learn some dog breeds!')} />
           <Buttons buttonText={'Test your skills and play a dog game'} buttonClass={"playgamebutton"} handleclick={() => alert('let\'s play a dog game!')} />
