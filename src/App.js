e31b6fa00ff88e80d09d19b74ff3189046a4901c
rickhekman <rickhekman@gmail.com>
2019-07-24 11:39:27 +0200

    Changed the button styling and updated the GitHub Cheat Sheet

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ function App() {
       <div className="App">
         <main>
           <h1>Dog Spirit Animal APP</h1>
-          <Buttons buttonText={'Learn dog breeds'} buttonClass={"learnbutton"} handleclick={() => alert('let\'s learn some dog breeds!')} />
-          <Buttons buttonText={'Test your skills and play a dog game'} buttonClass={"playgamebutton"} handleclick={() => alert('let\'s play a dog game!')} />
+          <div>
+            <Buttons buttonText={'Learn dog breeds'} buttonClass={"learnbutton"} handleclick={() => alert('let\'s learn some dog breeds!')} />
+            <Buttons buttonText={'Test your skills and play a dog game'} buttonClass={"playgamebutton"} handleclick={() => alert('let\'s play a dog game!')} />
+          </div>
         </main>
         <footer>
           <p>Made with ❤ by Bassam, Meryl and Rick</p>
