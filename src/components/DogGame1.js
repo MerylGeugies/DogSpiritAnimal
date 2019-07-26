10c560eba1ebb4215d1a44460f2ebb7f91211e19
rickhekman <rickhekman@gmail.com>
2019-07-26 12:09:46 +0200

    start dog game 1

---
+++ src/components/DogGame1.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import { connect } from 'net';
+//import { Link } from 'react-router-dom'
+
+export default function DogGame(props) {
+
+  return (
+    <div>
+      <h1>Test your skills in this dog game</h1>
+      <div>
+
+
+      </div>
+      {/* <ul>
+             { props.dogBreeds
+              ? props.dogBreeds.map(breed => <p key={breed}>{breed}</p>)
+             
+               : 'Loading...'
+            }
+          </ul> */}
+    </div>
+  );
+}
